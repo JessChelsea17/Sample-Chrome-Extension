f77295b8834b58a9b021205782b8e916f6f2527d
Jessica Carolina Rodriguez <jess.fresiamay@gmail.com>
2019-04-16 21:32:57 +0800

    Update background.js

--- background.js
+++ background.js
@@ -9,13 +9,46 @@
   function getToken(token) {
     
     console.log('this is the token: ', token);
+
+      var params = {
+        "range":"Sheet1!A1:B1",
+        "majorDimension": "ROWS",
+          "values": [
+          ["Hi","Crush"]
+        ],
+      }
+      let init = {
+        method: 'PUT',
+        async: true,
+        // data: JSON.stringify(params),
+        body: JSON.stringify(params),
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        }
+      };
+      fetch(
+          "https://sheets.googleapis.com/v4/spreadsheets/1efS6aMlPFqHJJdG8tQw-BNlv9WbA21jQlufsgtMsUmw/values/Sheet1!A1:B1?valueInputOption=USER_ENTERED",
+          init)
+          .then((response) => console.log(response))
+
+          let request = {
+            method: 'GET',
+            async: true,
+            headers: {
+              Authorization: 'Bearer ' + token,
+              'Content-Type': 'application/json'
+            },
+          };
+          fetch(
+              "https://sheets.googleapis.com/v4/spreadsheets/1efS6aMlPFqHJJdG8tQw-BNlv9WbA21jQlufsgtMsUmw/values/Sheet1!A1:B1",
+              request)
+              .then((response) => response.json())
+              .then(function(data) {
+                console.log(data)
+              });
   }
 
-  function displayPath(fileEntry) {
-    chrome.fileSystem.getDisplayPath(fileEntry, function(path) {
-      console.log(path)
-    });
-  }
 
   
 
